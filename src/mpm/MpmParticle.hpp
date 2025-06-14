32f7e33fb2a94625eaa98b3bc3403b016fa58206
pherrephatta <xenotimex@gmail.com>
2025-06-14 14:26:52 -0400

    Add initial volume to particles

--- src/mpm/MpmParticle.hpp
+++ src/mpm/MpmParticle.hpp
@@ -8,7 +8,7 @@ struct MpmParticle {
     vec3 velocity = vec3::Zero();               // v
     double density{0};                          // rho
     double mass{0};                             // m
-    double volume{0};                           // V
+    double volume_0{0};                           // V
     mat3 deform_elastic = mat3::Identity();     // F_E
     mat3 deform_plastic = mat3::Identity();     // F_P
 
