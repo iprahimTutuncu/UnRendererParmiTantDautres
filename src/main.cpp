3ec795049896a392c9cc7c803a081787601157c6
Samuel Lachance <leuchak@slachance.ca>
2025-06-25 20:28:24 -0400

    enable debug mode in SDL_CreateGPUDevice

--- src/main.cpp
+++ src/main.cpp
@@ -54,7 +54,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
         return SDL_APP_FAILURE;
     }
 
-    SDL_GPUDevice *device = SDL_CreateGPUDevice(SDL_GPU_SHADERFORMAT_SPIRV, false, "vulkan");
+    SDL_GPUDevice *device = SDL_CreateGPUDevice(SDL_GPU_SHADERFORMAT_SPIRV, true, "vulkan");
     if (device == nullptr) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Failed to create SDL GPU Device: %s", SDL_GetError());
         return SDL_APP_FAILURE;
