a586e0a1163d0784f14de5e452ca87392f1f8a8a
Samuel Lachance <leuchak@slachance.ca>
2025-07-14 15:42:50 -0400

    streamline texture management and remove unused texture code (#31)

--- src/graphics/deferred_gbuffer_renderer.cpp
+++ src/graphics/deferred_gbuffer_renderer.cpp
@@ -132,7 +132,7 @@ void deferred_gbuffer_render(AppState& state, SDL_GPUCommandBuffer* cmdBuf) {
 
     // Bind default texture
     SDL_GPUTextureSamplerBinding samplerBinding {};
-    samplerBinding.texture = graphics.textures[DefaultWhite];
+    samplerBinding.texture = graphics.staticTextures[DefaultWhite];
     samplerBinding.sampler = graphics.samplersPreset[LinearClamp];
 
     SDL_BindGPUFragmentSamplers(pass, 0, &samplerBinding, 1);
