11f98f15442643cf7eed124bb5c97f5c2e1e1292
Samuel Lachance <leuchak@slachance.ca>
2025-07-15 13:27:26 -0400

    refactor: simplify SDL include directives for better cross-platform compatibility

--- boreas/src/core/Window.hpp
+++ boreas/src/core/Window.hpp
@@ -1,13 +1,9 @@
 #pragma once
 
 #include <glad/glad.h>
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
-#include <SDL.h>
-#include <SDL_opengl>
-#else
+
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_opengl.h>
-#endif
 
 #include <atomic>
 
