d3602ed01324a7e0b0df8b2a4ca2e2ac8cd06a5b
Samuel Lachance <leuchak@slachance.ca>
2025-07-17 08:27:00 -0400

    add MPM data structures and simulation functions

--- src/physics/mpm.cpp
+++ src/physics/mpm.cpp
@@ -2,12 +2,21 @@
 
 #include <cmath>
 #include <limits>
+#include <vector>
 
 // c'est une tentative de comprendre MPM en l'implémentant moi même depuis les
 // référence donné par Ferat. ça va me permettre de mieux saisir quoi fait quoi et
 // d'écrire le code de manière à pouvoir l'optimiser.
 
 typedef float T;
+
+namespace constants {
+
+    static inline constexpr T poisson_ratio = static_cast<T>(0.3);
+    static inline constexpr T youngs_modulus = static_cast<T>(1e6);
+    static inline constexpr T initial_density = static_cast<T>(1000);
+
+}
 
 namespace basis {
 
@@ -99,6 +108,96 @@ namespace basis {
 
 namespace elastic {
 
-
-
 } // namespace elastic
+
+namespace mpm {
+
+    struct vec3_u {
+        float x;
+        float y;
+        float z;
+    };
+
+    struct GridNode {
+        vec3 position; // world space position of the node
+        vec3 velocity; // velocity of the node
+        vec3 force; // force applied to the node
+        float mass; // mass of the node
+    };
+
+    struct Grid {
+        T* x;
+        T* y;
+        T* z;
+        vec3* v;
+        vec3* flip;
+        T* mass;
+        T* friction;
+        std::vector<T> _raw_data;
+
+        size_t size() const {
+            return _raw_data.size();
+        }
+    };
+
+    struct Particles {
+
+        inline vec3* position() {
+            return reinterpret_cast<vec3*>(_raw_data.data());
+        };
+        inline vec3* velocity() {
+            return reinterpret_cast<vec3*>(_raw_data.data() + (sizeof(vec3) / sizeof(T)) * n);
+        };
+        inline mat3* F() {
+            return reinterpret_cast<mat3*>(_raw_data.data() + 2 * (sizeof(vec3) / sizeof(T)) * n);
+        };
+        inline mat3* B() {
+            return reinterpret_cast<mat3*>(_raw_data.data() + (2 * (sizeof(vec3) / sizeof(T)) + (sizeof(mat3) / sizeof(T))) * n);
+        };
+
+        Particles(size_t n) {
+            constexpr size_t size = 0
+                + sizeof(vec3) // position
+                + sizeof(vec3) // velocity
+                + sizeof(mat3) // F
+                + sizeof(mat3); // B
+
+            this->n = n;
+            _raw_data.resize(size * n);
+        }
+
+        size_t n; // number of particles
+        std::vector<float> _raw_data;
+    };
+
+    struct MpmState {
+        GridNode grid;
+        Particles particles;
+
+        float dt;
+        float mu;
+        float lambda;
+    };
+
+    void iterate(MpmState& state);
+    void simulate() {
+
+        MpmState state {
+            .particles = Particles(1000), // example size
+            .dt = 0.01f, // example time step
+            .mu = constants::youngs_modulus / (2 * (1 + constants::poisson_ratio)),
+            .lambda = (constants::youngs_modulus * constants::poisson_ratio) / ((1 + constants::poisson_ratio) * (1 - 2 * constants::poisson_ratio))
+        };
+
+        while (true) {
+            iterate(state);
+        }
+    }
+
+    void iterate(MpmState& state) {
+        // Rasterize particle data to grid
+
+        GridNode grid {};
+    }
+
+} // namespace mpm
